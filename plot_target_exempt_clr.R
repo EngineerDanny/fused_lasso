e6265d8dbdb0b4ec83abdea4bd9e8c8b31a198c1
EngineerDanny <agyapongdaniel7777@gmail.com>
2026-09-05 02:49:14 -0700

    Shorten CLR plot caption

--- plot_target_exempt_clr.R
+++ plot_target_exempt_clr.R
@@ -72,7 +72,7 @@ gg <- ggplot(plot.dt, aes(comparison, mse_difference)) +
   scale_x_discrete(NULL) +
   labs(
     title = "Prediction performance after target exempt CLR",
-    caption = "Values below zero favor fuser. Panels use different y-axis scales."
+    caption = "Values below zero favor fuser."
   )
 
 ggsave(
